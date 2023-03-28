cdae24687e1316b58a80a4db9badcb859c081271
Driss <drissaitkassi@gmail.com>
2023-03-28 15:50:46 +0000

    quick react bootstrap user interface

---
+++ src/titleSection.js
@@ -0,0 +1,19 @@
+import { Container } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function TitleSection(){
+
+    return(
+
+        <div id="myTitle">
+            <Container>
+                <h1>title section </h1>
+            </Container>
+        </div>
+      
+        
+    )
+}
+
+
+export default TitleSection;
